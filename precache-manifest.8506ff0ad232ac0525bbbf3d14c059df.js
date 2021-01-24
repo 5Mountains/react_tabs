c2a534fce09e2c0353c09097c6532c44a4144601
5Mountains <V.Piatyhor@gmail.com>
2021-01-25 00:08:19 +0200

    Updates

---
+++ precache-manifest.8506ff0ad232ac0525bbbf3d14c059df.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "72c6cf7a334071e6de93968154b83be1",
+    "url": "./index.html"
+  },
+  {
+    "revision": "71f5f826814cb3886202",
+    "url": "./static/css/main.f66bad44.chunk.css"
+  },
+  {
+    "revision": "205453e9777d64e69b03",
+    "url": "./static/js/2.bd866163.chunk.js"
+  },
+  {
+    "revision": "8a137f586fcebb1a370343eb47b7677c",
+    "url": "./static/js/2.bd866163.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "71f5f826814cb3886202",
+    "url": "./static/js/main.caf9f3da.chunk.js"
+  },
+  {
+    "revision": "53d1114eedfc0eaca354",
+    "url": "./static/js/runtime-main.a6df9e19.js"
+  }
+]);
